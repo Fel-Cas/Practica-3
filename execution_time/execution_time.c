a55841084c348efe0f5913579225a282dcd9bbcf
Andrés Castro <andres.cmonsalve@gmail.com>
2023-04-14 22:46:51 -0500

    Feat: Se cambió la manera de importar los archivos

--- execution_time/execution_time.c
+++ execution_time/execution_time.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include "time.h"
 
 void get_execution_time(struct timeval start, struct timeval end) {
-    double elapsed_time = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+    double elapsed_time = ((double)(end.tv_usec - start.tv_usec) /1000000 + (double)(end.tv_sec - start.tv_sec));
      printf("Tiempo de ejecución: %f microsegundos\n", elapsed_time);
 }
