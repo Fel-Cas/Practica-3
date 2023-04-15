c51adff228826ac1258e3b23c99e91fed9ee4a48
Santiago Jaramillo Ocampo <jaramilloocamposantiago@gmail.com>
2023-04-15 15:37:54 -0500

    Feat: Add matrices with paralellism. Op9 done.

--- matrices_sum/matrices_sum.c
+++ matrices_sum/matrices_sum.c
@@ -3,8 +3,44 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include "../execution_time/time.h"
-#include "./matrices_sum.h"
 #include <pthread.h>
+#include "../matrix/matrix.h"
+
+void add_two_matrices(int m1_rows, int m1_cols, int m2_rows, int m2_cols, int n);
+void add_matrices_without_paralellism(Matrix *M, Matrix *N);
+void add_matrices_with_paralellism(Matrix *M, Matrix *N, int n);
+
+// Estructura de datos para pasar los argumentos a la funcion que realiza la multiplicacion de la matriz por un escalar usando paralelismo
+typedef struct
+{
+    Matrix *R;
+    Matrix *M;
+    Matrix *N;
+    int start_row;
+    int end_row;
+    pthread_mutex_t *mutex;
+} MatricesSumArgs;
+
+// Funcion que realiza la suma de las matrices usando paralelismo
+void *add_matrices_parallel(void *arg)
+{
+    MatricesSumArgs *args = (MatricesSumArgs *)arg;
+    if (args->M->rows != args->N->rows || args->M->cols != args->N->cols)
+    {
+        fprintf(stderr, "Invalid size. (%d, %d) and (%d, %d)\n", args->M->rows, args->M->cols, args->N->rows, args->N->cols);
+        return NULL;
+    }
+    for (int i = args->start_row; i < args->end_row; ++i)
+    {
+        for (int j = 0; j < args->M->cols; ++j)
+        {
+            pthread_mutex_lock(args->mutex);
+            args->R->elements[i][j] = args->M->elements[i][j] + args->N->elements[i][j];
+            pthread_mutex_unlock(args->mutex);
+        }
+    }
+    return NULL;
+}
 
 void add_two_matrices(int m1_rows, int m1_cols, int m2_rows, int m2_cols, int n)
 {
@@ -22,8 +58,79 @@ void add_two_matrices(int m1_rows, int m1_cols, int m2_rows, int m2_cols, int n)
     // Se imprime la matriz 2
     print_matrix(N);
 
-    Matrix* R=add_matrix(M,N);
-    print_matrix(R);
+    // Suma de matrices sin paralelismo
+    add_matrices_without_paralellism(M, N);
+
+    // Suma de matrices con paralelismo
+    add_matrices_with_paralellism(M, N, n);
 
     free_matrix(M);
+    free_matrix(N);
 }
+
+// Funcion para realizar la suma de la matriz  sin usar paralelismo
+void add_matrices_without_paralellism(Matrix *M, Matrix *N)
+{
+    printf("\nSum of matrices without paralellism\n");
+    // Se obtiene el tiempo de inicio
+    struct timeval start, end;
+    gettimeofday(&start, 0);
+    // Suma de matrices sin paralelismo
+    Matrix *R = add_matrix(M, N);
+    // Se obtiene el tiempo de finalizacion
+    gettimeofday(&end, 0);
+    // Se imprime el tiempo de ejecucion y el resultado de la multiplicación
+    get_execution_time(start, end);
+    print_matrix(R);
+    free_matrix(R);
+}
+
+
+void add_matrices_with_paralellism(Matrix *M, Matrix *N, int n)
+{
+    printf("\nParallel sum of matrices with paralellism\n");
+    struct timeval start, end;
+    const int num_threads = n;                   // Número de hilos
+    Matrix *R = create_matrix(M->rows, M->cols); // Matriz con el resultado de la suma
+    // Se crea un arreglo de hilos
+    pthread_t threads[num_threads];
+    // se inicializa la estructura de datos con el número de hilos a utilizar
+    MatricesSumArgs thread_args[num_threads];
+    // se inicializa el lock
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    // Se obtiene el tiempo de inicio
+    gettimeofday(&start, 0);
+    // Se divide la matriz en partes iguales para cada hilo
+    const int chunk_size = M->rows / num_threads;
+    // Se le asigna a cada hilo una parte de la matriz
+    int start_row = 0;
+    for (int i = 0; i < num_threads; ++i)
+    {
+        int end_row = start_row + chunk_size;
+        if (i == num_threads - 1)
+        {
+            end_row = M->rows;
+        }
+        // Se inicializan los datos necesarios para cada hilo
+        thread_args[i] = (MatricesSumArgs){.R = R, .M = M, .N = N, .start_row = start_row, .end_row = end_row, .mutex = &mutex};
+        pthread_create(&threads[i], NULL, add_matrices_parallel, &thread_args[i]);
+        start_row = end_row;
+    }
+
+    // Esperamos a que todos los hilos terminen
+    for (int i = 0; i < num_threads; ++i)
+    {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_mutex_destroy(&mutex); // Destruimos el mutex
+    // Se obtiene el tiempo de finalizacion
+    gettimeofday(&end, 0);
+    // Se imprime el tiempo de ejecucion y el resultado de la multiplicación
+    get_execution_time(start, end);
+    // Se imprime la matriz resultado
+    print_matrix(R);
+    // Se libera la memoria de las matrices
+    free_matrix(R);
+}
+
--- matrices_sum/matrices_sum.h
+++
@@ -1,5 +0,0 @@
-#ifndef MATRIX_SCALAR_H
-#define MATRIX_SCALAR_H
-#include "../matrix/matrix.h"
-void add_two_matrices(int m1_rows, int m1_cols, int m2_rows, int m2_cols, int n);
-#endif
