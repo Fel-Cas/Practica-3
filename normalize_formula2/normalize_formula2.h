14d180bfecb7b8284bc3ecbf4bd05997d5e45d6a
Andrés Castro <andres.cmonsalve@gmail.com>
2023-04-15 21:07:33 -0500

    Feat: Se refactorizó la manera de paralelizar la normalización

--- normalize_formula2/normalize_formula2.h
+++ normalize_formula2/normalize_formula2.h
@@ -3,5 +3,5 @@
 #include "../matrix/matrix.h"
 void main_normalize_formula_2(int rows, int cols, int n);
 void execute_normalize_formula_2_normal(Matrix* M, Vector* vrz, Vector* std);
-void execute_normalize_formula_2_parallel(Matrix* M, Vector* vrz, Vector* std, int n);
+void normalize_matrix_column_formula_2_parallel(Matrix* M, Vector* vrz, Vector* std, int num_threads);
 #endif //FORMULA2_H
