bfe66103914e68326b35ad2d33a433d95c5cfbd1
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-04-28 19:08:45 -0500

    Fix: cambiando el nombre de la clase principal

--- matrices.c
+++ matrices.c
@@ -4,62 +4,73 @@
 #include "./matrix/matrix.c"
 #include "./execution_time/execution_time.c"
 #include "./params/parametros.c"
-#include "./standard_deviation/standard_deviation.c"
 #include "./matrix_scalar/matrix_scalar.c"
 #include "./normalize_formula1/normalize_formula1.c"
 #include "./normalize_formula2/normalize_formula2.c"
 #include "./file/file.c"
 #include "./matrices_sum/matrices_sum.c"
 #include "./matrices_dot/matrices_dot.c"
+#include "./validations/validations.c"
 #include "./column_mean/column_mean.c"
 #include "./column_variance/column_variance.c"
+#include "./utils/minorValue.c"
+#include "./standard_deviation/standard_deviation.c"
+#include "./min_max/min_max.c"
 
-int main(int argc, char *argv[]){
-    int o=0,f=0,c=0,r=0,s=0,n=0;
+
+int select_operation(int o, int f, int c, int r, int s, double e, int n, int file);
+
+int main(int argc, char *argv[])
+{
+    int o = 0, f = 0, c = 0, r = 0, s = 0, n = 0, file = 0;
     char p[200] = "";
-    double e=0;
-    get_params(argc,argv,&o,&f,&c,&e,&r,&s,p,&n);
-    if(strcmp(p, "") == 0){
-        switch(o){
-            case 1:
-                matrix_columns_mean(f,c,n);
-                return 0;
-            case 2:
-                matrix_columns_variance(f,c,n);
-                return 0;
-            case 3:
-                calculate_standard_deviation_by_column(f, c, n);
-                return 0;
-            case 4:
-                return 0;
-            case 5:
-                add_two_matrices(f,c,r,s,n);
-                return 0;
-            case 6:
-                dot_two_matrices(f,c,r,s,n);
-                printf("hola");
-                return 0;
-            case 7:
-                 multiply_matrix_by_scalar(f,c,e,n);
-               return 0;
-            case 8:
-                  main_normalize_formula_1(f,c,n);  
-                return 0;
-            case 9:
-                  main_normalize_formula_2(f,c,n);
-                return 0;
-            default:
-                printf("Operacion no valida %d ",o);
-                exit(EXIT_FAILURE);
-            }
-        }
-    printf("p = %s\n", p);
-    read_file(&o,&f, &c, &e, &r, &s, p,&n);
-    printf("The value of o is %d\n", o);
-    printf("The value of f is %d\n", f);
-    printf("The value of c is %d\n", c);
-    Matrix *matrix1 = create_matrix_from_file("op1.txt", f, c);
-    print_matrix(matrix1);
+    double e = 0;
+    get_params(argc, argv, &o, &f, &c, &e, &r, &s, p, &n);
+    if (strcmp(p, "") == 0)
+    {
+        select_operation(o, f, c, r, s, e, n, file);
+    }
+    else
+    {
+        file = 1;
+        read_file(&o, &f, &c, &e, &r, &s, p, &n);
+        select_operation(o, f, c, r, s, e, n, file);
+    }
     return 0;
 }
-
+int select_operation(int o, int f, int c, int r, int s, double e, int n, int file)
+{
+    switch (o)
+    {
+    case 1:
+        matrix_columns_mean(f, c, n, file);
+        return 0;
+    case 2:
+        matrix_columns_variance(f, c, n, file);
+        return 0;
+    case 3:
+        calculate_standard_deviation_by_column(f, c, n, file);
+        return 0;
+    case 4:
+        calculate_min_max_by_columns(f, c, n);
+        return 0;
+    case 5:
+        add_two_matrices(f, c, r, s, n, file);
+        return 0;
+    case 6:
+        dot_two_matrices(f, c, r, s, n, file);
+        return 0;
+    case 7:
+        multiply_matrix_by_scalar(f, c, e, n, file);
+        return 0;
+    case 8:
+        main_normalize_formula_1(f, c, n, file);
+        return 0;
+    case 9:
+        main_normalize_formula_2(f, c, n, file);
+        return 0;
+    default:
+        printf("Operacion no valida %d ", o);
+        exit(EXIT_FAILURE);
+    }
+}
